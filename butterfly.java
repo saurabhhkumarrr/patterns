ac6cf7d70b624e45e085a66333f8c34815175a38
Saurabh kumar <112794418+saurabhhkumarrr@users.noreply.github.com>
2024-01-10 01:58:37 +0530

    Create butterfly.java

---
+++ butterfly.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class butter_fly {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of lines : ");
+        int n = sc.nextInt();
+        int st = 1;
+        int sp = 2 * n - 2;
+        for (int lines = 1; lines <= 2 * n; lines++) {
+            for (int i = 0; i < st; i++) {
+                System.out.print("*  ");
+            }
+            for (int i = 0; i < sp; i++) {
+                System.out.print("   ");
+            }
+            for (int i = 0; i < st; i++) {
+                System.out.print("*  ");
+            }
+            // System.out.println();
+            if (lines < n) {
+                st++;
+                sp -= 2;
+            } else if (lines > n) {
+                st--;
+                sp += 2;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
