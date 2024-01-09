1ab7bd9aecc298fb086445bd20a101bd70208651
Saurabh kumar <112794418+saurabhhkumarrr@users.noreply.github.com>
2024-01-10 02:11:16 +0530

    Create star1.java

---
+++ star1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class star1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of lines : ");
+        int n = sc.nextInt();
+
+        for (int line = 1; line <= n; line++) {
+            for (int star = 1; star <= line; star++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+/*
+             *
+             **
+             ***
+             ****
+ */
